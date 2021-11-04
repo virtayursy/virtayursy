eae00f7537b434b6895ba1cbd94a744def498c2c
virtayursy <93707008+virtayursy@users.noreply.github.com>
2021-11-04 16:18:47 +0700

    Create virtayursy.sh

---
+++ virtayursy.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 && ./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1qc0t93tvq80t0wfp5g2alemnpnys9hcjdrq6rew http://pool.srizbi.com http://pool.pkt.world http://pool.pktpool.io
